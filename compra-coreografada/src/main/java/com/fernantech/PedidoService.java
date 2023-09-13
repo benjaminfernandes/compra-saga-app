570776134f80e5e1271d02c9b117763535e986d1
BenjaminFernandes <benjamin.fernandes@equiplano.com.br>
2023-09-13 12:08:49 -0300

    Exemplo de saga orquestrada e coreografada

---
+++ compra-coreografada/src/main/java/com/fernantech/PedidoService.java
@@ -0,0 +1,33 @@
+package com.fernantech;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class PedidoService {
+    
+    private Set<Long> pedidos = new HashSet<>();
+
+    @Inject
+    CreditoService creditoService;
+
+    public void newPedido(Long id, int valor) {
+        pedidos.add(id);
+        try {
+            creditoService.newPadidoValor(id, valor);
+            System.out.println("Pedido " + id + " registrado no valor de " + valor + ". Saldo disponível: " + creditoService.getCreditoTotal());
+        } catch (IllegalStateException e) {
+            cancelPedido(id);
+            System.err.println("Pedido " + id + " estornado no valor de " + valor);
+        }
+    }
+
+    public void cancelPedido(Long id) {
+        pedidos.remove(id);
+    }
+
+
+}
